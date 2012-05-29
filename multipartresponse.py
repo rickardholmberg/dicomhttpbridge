510b01fdc7720889d176aae5cb2d36357d5d151e
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2012-05-29 22:00:47 +0200

    Added exercise to send multiple response bodies with HTTP

---
+++ multipartresponse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from bottle import route, run, request, response, install
+import uuid
+
+@route("/")
+def returnmultipart():
+    boundary='----multipart-boundary-%s----' % (uuid.uuid1(),)
+    response.content_type = 'multipart/mixed; boundary=%s' % (boundary,)
+    s = boundary + "\n"
+    s += 'Content-Disposition: attachment filename="hej"\n'
+    s += '\n'
+    s += '<html><head/><body>HEj!</body></html>\n'
+    s += boundary + '\n'
+
+    return s
+    
+
+run(host='localhost', port=5000)

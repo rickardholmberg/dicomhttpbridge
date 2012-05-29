2428c0a95bdec5e19d00f0a85136a0dba0f495a3
Rickard Holmberg <rickard@holmberg.info>
2012-05-29 23:19:15 +0200

    And much nicer.

--- multipartresponse.py
+++ multipartresponse.py
@@ -2,20 +2,25 @@
 
 from bottle import route, run, request, response, install
 import uuid
+import glob
 
 @route("/")
 def returnmultipart():
     boundary='----multipart-boundary-%s----' % (uuid.uuid1(),)
     response.content_type = 'multipart/mixed; boundary=%s' % (boundary,)
-    s = boundary + "\n"
-    s += 'Content-Disposition: attachment; filename="hej";\n'
-    s += '\n'
-    s += '<html><head/><body>HEj!</body></html>\n'
-    s += boundary + "\n"
-    s += 'Content-Disposition: attachment; filename="hej2";\n'
-    s += '\n'
-    s += '<html><head/><body>HEj2!</body></html>\n'
-    s += boundary + '\n'
+    s = ''
+
+    for fn in glob.glob("*.dcm"):
+        print fn
+        s += "\n" + boundary + "\n"
+        s += 'Content-Disposition: attachment; filename="%s";\n' % (fn,)
+        s += 'Content-Type: application/dicom;\n'
+        q = file(fn).read()
+        s += 'Content-Length: %i\n' % (len(q),)
+        s += '\n'
+        s += q
+
+    s += '\n' + boundary + '\n'
 
     return s
     

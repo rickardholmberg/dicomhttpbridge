0dc9c8351c32c1984528ed43ce906f7c1897cbaa
Rickard Holmberg <rickard@holmberg.info>
2012-05-30 12:17:13 +0200

    multipartresponse now does arbitrarily large attachments with constant memory use.

--- catio.py
+++ catio.py
@@ -1,18 +1,41 @@
+import StringIO
+
 class CatIO(object):
     def __init__(self):
         self._bufs = []
-        self._curbuf = None
+        self._curbuf = 0
         self._curpos = 0
+        self._closed = False
+        self.softspace = 0
 
     def __iadd__(self, x):
+        if isinstance(x, basestring):
+            return self.__iadd__(StringIO.StringIO(x))
         self._bufs.append(x)
         return self
 
+    def flush(self):
+        return
+
+    def next(self):
+        raise NotImplementedError()
+
+    def readline(self, size = None):
+        raise NotImplementedError()
+    def readlines(self, sizehint = None):
+        raise NotImplementedError()
+    @property
+    def closed(self):
+        return self._closed
+    @property
+    def newlines(self):
+        return None
+
     def read(self, size = None):
-        if self._curbuf == None:
-            if len(self._bufs) == 0:
-                return ''
-            self._curbuf = 0
+        if self._closed:
+            raise ValueError()
+        if len(self._bufs) == 0:
+            return ''
         if size == None:
             r = self._bufs[self._curbuf].read()
         else:
@@ -23,9 +46,57 @@ class CatIO(object):
         if self._curbuf >= len(self._bufs)-1:
             return r
         self._curbuf += 1
-        return r + self.read(size - len(r))
+        if self._curbuf < len(self._bufs):
+            self._bufs[self._curbuf].seek(0)
+        if size == None:
+            return r + self.read()
+        else:
+            return r + self.read(size - len(r))
 
     def tell(self):
+        if self._closed:
+            raise ValueError()
         return self._curpos
+
+    def write(self, *args, **kwargs):
+        if self._closed:
+            raise ValueError()
+        raise IOError('File not open for writing',)
         
-        
+    def seek(self, offset, whence=0):
+        if self._closed:
+            raise ValueError()
+        if len(self._bufs) == 0:
+            return
+        if whence == 0:
+            self._curbuf = 0
+            self._curpos = 0
+            if offset < 0:
+                offset = 0
+            while True:
+                self._bufs[self._curbuf].seek(0, 2)
+                curlen = self._bufs[self._curbuf].tell()
+                if offset <= self._curpos + curlen:
+                    self._bufs[self._curbuf].seek(offset - self._curpos, 0)
+                    self._curpos = offset
+                    return
+                else:
+                    self._curpos += curlen
+                    if self._curbuf < len(self._bufs)-1:
+                        self._curbuf += 1
+                    else:
+                        break
+        elif whence == 1:
+            return self.seek(self._curpos + offset)
+        elif whence == 2:
+            curbufpos = self._bufs[self._curbuf].tell()
+            self._bufs[self._curbuf].seek(0)
+            self._curpos -= curbufpos
+            while self._curbuf < len(self._bufs):
+                self._bufs[self._curbuf].seek(0,2)
+                self._curpos += self._bufs[self._curbuf].tell()
+                self._curbuf += 1
+            self._curbuf -= 1
+            return self.seek(offset, 1)
+        else:
+            raise IOError(22, 'Invalid argument')
--- multipartresponse.py
+++ multipartresponse.py
@@ -4,12 +4,13 @@ from bottle import route, run, request, response, install
 import uuid
 import glob
 import os
+import catio
 
 @route("/")
 def returnmultipart():
     boundary='----multipart-boundary-%s----' % (uuid.uuid1(),)
     response.content_type = 'multipart/mixed; boundary=%s' % (boundary,)
-    s = ''
+    s = catio.CatIO()
 
     for fn in glob.glob("*.dcm"):
         print fn
@@ -18,7 +19,7 @@ def returnmultipart():
         s += 'Content-Type: application/dicom;\r\n'
         s += 'Content-Length: %i\r\n' % (os.stat(fn).st_size,)
         s += '\r\n'
-        s += file(fn).read()
+        s += file(fn)
 
     s += '\r\n' + boundary + '\r\n'
 

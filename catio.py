531241ad40a857d1851708680441cf9219e2c3a8
Rickard Holmberg <rickard@holmberg.info>
2012-05-30 10:22:42 +0200

    Adding CatIO for representing multiple file-like objects as one.

---
+++ catio.py
@@ -0,0 +1,31 @@
+class CatIO(object):
+    def __init__(self):
+        self._bufs = []
+        self._curbuf = None
+        self._curpos = 0
+
+    def __iadd__(self, x):
+        self._bufs.append(x)
+        return self
+
+    def read(self, size = None):
+        if self._curbuf == None:
+            if len(self._bufs) == 0:
+                return ''
+            self._curbuf = 0
+        if size == None:
+            r = self._bufs[self._curbuf].read()
+        else:
+            r = self._bufs[self._curbuf].read(size)
+        self._curpos += len(r)
+        if size != None and len(r) == size:
+            return r
+        if self._curbuf >= len(self._bufs)-1:
+            return r
+        self._curbuf += 1
+        return r + self.read(size - len(r))
+
+    def tell(self):
+        return self._curpos
+        
+        
